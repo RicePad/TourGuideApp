2d9244355c8288c2cbfc1cc2adae3c220a14ec78
JON <jonn.thinkrtc@gmail.com>
2018-08-20 15:09:45 -0400

    Created 4 new empty activities, and added even listerners with intents.

--- app/src/main/java/com/udacity/jonathan/tourorlandoapp/MainActivity.java
+++ app/src/main/java/com/udacity/jonathan/tourorlandoapp/MainActivity.java
@@ -1,7 +1,11 @@
 package com.udacity.jonathan.tourorlandoapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,68 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        // Find the View that shows the  category
+        TextView themeParks = findViewById(R.id.theme_park_list);
+        TextView restaurantList = findViewById(R.id.restaurant_list);
+        TextView hotelList = findViewById(R.id.hotel_list);
+        TextView nightClubList = findViewById(R.id.night_clubs_list);
+
+
+
+        // Set a click listener on the desire View
+        themeParks.setOnClickListener(new OnClickListener() {
+            // The code in this method will be executed when the numbers category is clicked on.
+            @Override
+            public void onClick(View view) {
+                // Create a new intent to open the {@link NumbersActivity}
+                Intent themeParksIntent = new Intent(MainActivity.this, ThemeParksActivity.class);
+
+                // Start the new activity
+                startActivity(themeParksIntent);
+            }
+        });
+
+        // Set a click listener on the desire View
+        restaurantList.setOnClickListener(new OnClickListener() {
+            // The code in this method will be executed when the numbers category is clicked on.
+            @Override
+            public void onClick(View view) {
+                // Create a new intent to open the {@link NumbersActivity}
+                Intent restaurantListIntent = new Intent(MainActivity.this, RestaurantActivity.class);
+
+                // Start the new activity
+                startActivity(restaurantListIntent);
+            }
+        });
+
+        // Set a click listener on the desire View
+        hotelList.setOnClickListener(new OnClickListener() {
+            // The code in this method will be executed when the numbers category is clicked on.
+            @Override
+            public void onClick(View view) {
+                // Create a new intent to open the {@link NumbersActivity}
+                Intent hotelListIntent = new Intent(MainActivity.this, HotelActivity.class);
+
+                // Start the new activity
+                startActivity(hotelListIntent);
+            }
+        });
+
+        // Set a click listener on the desire View
+        nightClubList.setOnClickListener(new OnClickListener() {
+            // The code in this method will be executed when the numbers category is clicked on.
+            @Override
+            public void onClick(View view) {
+                // Create a new intent to open the {@link NumbersActivity}
+                Intent nightClubListIntent = new Intent(MainActivity.this, NightClubActivity.class);
+
+                // Start the new activity
+                startActivity(nightClubListIntent);
+            }
+        });
+
+
     }
 }
