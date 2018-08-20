7e6934c6fa39198b484c1ce23777b97671a50e5a
JON <jonn.thinkrtc@gmail.com>
2018-08-20 15:36:40 -0400

    Created ThemeParks fragment

--- app/src/main/java/com/udacity/jonathan/tourorlandoapp/ThemeParksActivity.java
+++ app/src/main/java/com/udacity/jonathan/tourorlandoapp/ThemeParksActivity.java
@@ -8,6 +8,10 @@ public class ThemeParksActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_theme_parks);
+        setContentView(R.layout.activity_category);
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.container, new ThemeParksFragment())
+                .commit();
+
     }
 }
