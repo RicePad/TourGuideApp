e39c6ac0698747fba68274e2b2e7eb4ce1ebbfc9
JON <jonn.thinkrtc@gmail.com>
2018-08-21 11:37:13 -0400

    refactor ThemeParkFragment

--- app/src/main/java/com/udacity/jonathan/tourorlandoapp/ThemeParksFragment.java
+++ app/src/main/java/com/udacity/jonathan/tourorlandoapp/ThemeParksFragment.java
@@ -11,6 +11,8 @@ import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 /**
  * A simple {@link Fragment} subclass.
  */
@@ -22,38 +24,28 @@ public class ThemeParksFragment extends Fragment {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.attraction_list, container, false);
 
         // Create a list of words
-        final ArrayList<Word> words = new ArrayList<Word>();
-        words.add(new Word(R.string.number_one, R.string.miwok_number_one,
-                R.drawable.number_one, R.raw.number_one));
-        words.add(new Word(R.string.number_two, R.string.miwok_number_two,
-                R.drawable.number_two, R.raw.number_two));
-        words.add(new Word(R.string.number_three, R.string.miwok_number_three,
-                R.drawable.number_three, R.raw.number_three));
-        words.add(new Word(R.string.number_four, R.string.miwok_number_four,
-                R.drawable.number_four, R.raw.number_four));
-        words.add(new Word(R.string.number_five, R.string.miwok_number_five,
-                R.drawable.number_five, R.raw.number_five));
-        words.add(new Word(R.string.number_six, R.string.miwok_number_six,
-                R.drawable.number_six, R.raw.number_six));
-        words.add(new Word(R.string.number_seven, R.string.miwok_number_seven,
-                R.drawable.number_seven, R.raw.number_seven));
-        words.add(new Word(R.string.number_eight, R.string.miwok_number_eight,
-                R.drawable.number_eight, R.raw.number_eight));
-        words.add(new Word(R.string.number_nine, R.string.miwok_number_nine,
-                R.drawable.number_nine, R.raw.number_nine));
-        words.add(new Word(R.string.number_ten, R.string.miwok_number_ten,
-                R.drawable.number_ten, R.raw.number_ten));
+        final ArrayList<Attraction> attractions = new ArrayList<Attraction>();
+
+        attractions.add(new Attraction(R.string.number_one, R.string.magic_kingdom,
+                R.drawable.number_one));
+        attractions.add(new Attraction(R.string.number_one, R.string.magic_kingdom,
+                R.drawable.number_one));
+        attractions.add(new Attraction(R.string.number_one, R.string.magic_kingdom,
+                R.drawable.number_one));
+        attractions.add(new Attraction(R.string.number_one, R.string.magic_kingdom,
+                R.drawable.number_one));
+        attractions.add(new Attraction(R.string.number_one, R.string.magic_kingdom,
+                R.drawable.number_one));
 
         // Create an {@link WordAdapter}, whose data source is a list of {@link Word}s. The
         // adapter knows how to create list items for each item in the list.
-        WordAdapter adapter = new WordAdapter(getActivity(), words, R.color.category_numbers);
+        AttractionAdapter adapter = new AttractionAdapter(getActivity(), attractions, R.color.category_numbers);
 
         // Find the {@link ListView} object in the view hierarchy of the {@link Activity}.
         // There should be a {@link ListView} with the view ID called list, which is declared in the
@@ -67,34 +59,13 @@ public class ThemeParksFragment extends Fragment {
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
-                Word word = words.get(position);
-                mMediaPlayer = MediaPlayer.create(getActivity(), word.getAudioResourceId());
-                mMediaPlayer.start();
+                Attraction word = attractions.get(position);
 
-                // Setup a listener on the media player, so that we can stop and release the
-                // media player once the sound has finished playing.
-                mMediaPlayer.setOnCompletionListener(mCompletionListener);
             }
         });
 
         return rootView;
 
-    }
-    /**
-     * Clean up the media player by releasing its resources.
-     */
-    private void releaseMediaPlayer() {
-        // If the media player is not null, then it may be currently playing a sound.
-        if (mMediaPlayer != null) {
-            // Regardless of the current state of the media player, release its resources
-            // because we no longer need it.
-            mMediaPlayer.release();
-
-            // Set the media player back to null. For our code, we've decided that
-            // setting the media player to null is an easy way to tell that the media player
-            // is not configured to play an audio file at the moment.
-            mMediaPlayer = null;
-        }
     }}
 
-    }}
+
