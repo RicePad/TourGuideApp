40324c844e21126e259c1e83c19b9e8c4e35cb31
JON <jonn.thinkrtc@gmail.com>
2018-08-21 11:18:24 -0400

    finish refactoring AttractionAdapter

--- app/src/main/java/com/udacity/jonathan/tourorlandoapp/AttractionAdapter.java
+++ app/src/main/java/com/udacity/jonathan/tourorlandoapp/AttractionAdapter.java
@@ -1,7 +1,71 @@
 package com.udacity.jonathan.tourorlandoapp;
 
+import android.content.Context;
+import android.support.v4.content.ContextCompat;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class AttractionAdapter extends ArrayAdapter<Attraction> {
 
+    private int mColorResourceId;
+
+    public AttractionAdapter(Context context, ArrayList<Attraction> words, int colorResourceId) {
+        super(context, 0, words);
+        mColorResourceId = colorResourceId;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        // Check if an existing view is being reused, otherwise inflate the view
+        View listItemView = convertView;
+        if (listItemView == null) {
+            listItemView = LayoutInflater.from(getContext()).inflate(
+                    R.layout.list_item, parent, false);
+        }
+
+        // Get the {@link Word} object located at this position in the list
+        Attraction currentAttraction = getItem(position);
+
+        // Find the TextView in the list_item.xml layout with the ID attraction_text_view.
+        TextView attractionTextView = (TextView) listItemView.findViewById(R.id.attraction_text_view);
+        // Get the Attractiontitle from the currentAttraction object and set this text on
+        // the TextView.
+        attractionTextView.setText(currentAttraction.getAttractionTitle());
+
+        // Find the TextView in the list_item.xml layout with the ID default_text_view.
+        TextView defaultTextView = (TextView) listItemView.findViewById(R.id.default_text_view);
+        // Get the default translation from the currentAttraction object and set this text on
+        // the default TextView.
+        defaultTextView.setText(currentAttraction.getAttractionDescription());
+
+        // Find the ImageView in the list_item.xml layout with the ID image.
+        ImageView imageView = (ImageView) listItemView.findViewById(R.id.image);
+        // Check if an image is provided for this word or not
+        if (currentAttraction.hasImage()) {
+            // If an image is available, display the provided image based on the resource ID
+            imageView.setImageResource(currentAttraction.getImageResourceId());
+            // Make sure the view is visible
+            imageView.setVisibility(View.VISIBLE);
+        } else {
+            // Otherwise hide the ImageView (set visibility to GONE)
+            imageView.setVisibility(View.GONE);
+        }
+
+        // Set the theme color for the list item
+        View textContainer = listItemView.findViewById(R.id.text_container);
+        // Find the color that the resource ID maps to
+        int color = ContextCompat.getColor(getContext(), mColorResourceId);
+        // Set the background color of the text container View
+        textContainer.setBackgroundColor(color);
+
+        // Return the whole list item layout (containing 2 TextViews) so that it can be shown in
+        // the ListView.
+        return listItemView;
+    }
 }
